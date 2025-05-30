343bc6577ed08be105aadaeab9ac21dc8ca4c407
Sándor Balázs <sabuaai@INF.ELTE.HU>
2025-05-31 01:17:42 +0200

    Camera, Player, Move

--- Bulldozer/Assets/Scripts/PlayerMovementScript.cs
+++ Bulldozer/Assets/Scripts/PlayerMovementScript.cs
@@ -7,11 +7,28 @@ public class PlayerMovementScript : MonoBehaviour
 
     public float moveSpeed = 4f;
 
+    public Transform cameraTransform;
+
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        transform.position += new Vector3(horizontal, vertical) * Time.deltaTime * moveSpeed;
+        Vector3 moveDirection = new Vector3(horizontal, vertical);
+
+        transform.position += moveDirection * Time.deltaTime * moveSpeed;
+
+        if (horizontal > 0.01f)
+            transform.rotation = Quaternion.Euler(0f, 0f, 270f); // Right
+        else if (horizontal < -0.01f)
+            transform.rotation = Quaternion.Euler(0f, 0f, 90f); // Left
+
+        if (vertical > 0.01f)
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f); // Up
+        else if (vertical < -0.01f)
+            transform.rotation = Quaternion.Euler(0f, 0f, 180f); // Down
+
+        //Camera rotation reset.
+        cameraTransform.rotation = Quaternion.identity;
     }
-}
+};
