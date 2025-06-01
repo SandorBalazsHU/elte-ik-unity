9ffa7fcba9c5a5dffdc0a6d06e9f46d64dcf8199
Sándor Balázs <sabuaai@INF.ELTE.HU>
2025-06-01 22:14:48 +0200

    Add projectals

---
+++ Bulldozer/Assets/Scripts/ProjectileHandlerScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileHandlerScript : MonoBehaviour
+{
+    public Vector2 moveDirection = new Vector2(0, 2);
+
+    private SpriteRenderer sRenderer;
+    private bool isDestroying = false;
+
+    void Awake()
+    {
+        sRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(moveDirection.x, moveDirection.y) * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isDestroying)
+            StartCoroutine(DestroyAfterRed());
+    }
+
+    IEnumerator DestroyAfterRed()
+    {
+        isDestroying = true;
+        moveDirection = new Vector2(0, 0);
+        if (sRenderer != null)
+            sRenderer.color = Color.red;
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Projectal destroyed!");
+        Destroy(gameObject);
+    }
+}
