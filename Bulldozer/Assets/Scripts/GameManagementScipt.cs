2c1d32bb3e281c4e73d3fcb560752a03726ca816
Sándor Balázs <sabuaai@INF.ELTE.HU>
2025-06-02 02:08:20 +0200

    All mechanics ready 4

--- Bulldozer/Assets/Scripts/GameManagementScipt.cs
+++ Bulldozer/Assets/Scripts/GameManagementScipt.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 using TMPro;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 //SINGLETON
 public class GameManagementScipt : MonoBehaviour
